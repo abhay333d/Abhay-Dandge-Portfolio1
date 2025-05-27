f8e048baf8dfc4b99dec138c3b2f9efb158ae955
abhay333d <abhaydandge333@gmail.com>
2025-05-28 01:26:48 +0530

    added the required models and images and loaded the model in the scene

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import { OrbitControls, useGLTF } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
 import React from 'react'
 
 const App = () => {
+  const model = useGLTF('/models/avatar1.glb')
   return (
-    <div>
-      
-    </div>
+    <>
+      <Canvas>
+        <OrbitControls />
+        <directionalLight intensity={2} position={[10, 10, 10]} />
+        <pointLight intensity={2} position={[-10, -10, -10]} />
+        <ambientLight intensity={1} />
+        <primitive object={model.scene} scale={3} position-y={-2.5} />
+      </Canvas>
+    </>
   )
 }
 
